cc8b558d2242a16b984a4ceb34099fa9d4ba544a
Florian Gionnane <f.gionnane@stag.technofuturtic.education>
2025-03-25 08:39:14 +0100

    25/3/25 matin

--- Javascript/TechnoFuturTIC/Initiation Au Langage Javascript/Exo3.js
+++ Javascript/TechnoFuturTIC/Initiation Au Langage Javascript/Exo3.js
@@ -3,7 +3,7 @@ let numberOne = Number(prompt("Enter 1 number"))
 let numberTwo = Number(prompt("Enter a second number"))
 
 let resultat  = numberOne + numberTwo 
-document.write("Le résultat est " + resultat)
+document.write("Le résultat est: " + resultat)
 
 /*
 It seems that JS doesnt recongize automatically 5 and 7 as int type, if not converted it'll give a concatenated string (5 + 7 = 57)
